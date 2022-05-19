21c92ffad1c9db780ce0aa66778fa04043432011
uvsq21603504 <fadi.mechri@ens.uvsq.fr>
2022-05-19 02:35:34 +0200

    first

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require ("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'})
+
+const app = express();
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+});
+
+/*const publicDirectory = path.join(__dirname, './assets');
+app.use(express.static(publicDirectory));*/
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MYSQL Connected...")
+    }
+})
+
+
+//Define Routes
+app.use('/', require('./routes/pages'))
+
+app.use('/auth', require('./routes/auth'));
+
+app.listen(5000, () => {
+console.log("Server started on Port 5000");
+});
+
+app.use(express.static('./assets/css'));
+app.use(express.static('./assets/images'));
+app.use(express.static('./assets/js'));
+app.use(express.static('./assets/css/theme-color'));
